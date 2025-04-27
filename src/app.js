a6071e0c536441191f04fc13a60de0e6ec8f7b42
sasidhar <sasidharlolla8@gmail.com>
2025-04-27 11:40:18 +0530

    Dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Welcome to test");
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  // console.log(req.body);
+
+  const user = new User(req.body);
+
+  // const user = new User({
+  //   firstName: "Sreekar",
+  //   lastName: "Lolla",
+  //   emailId: "lsreekar@gmail.com",
+  //   password: "12345678",
+  // });
+
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user" + err.message);
+  }
 });
 
-app.listen(3000, (req, res) => {
-  console.log("Server is listening at port 3000");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server is listening at port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected");
+  });
